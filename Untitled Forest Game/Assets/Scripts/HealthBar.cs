1285bc15c9812c2615a1fe1cd285ac34019643ed
Winnie Sich <wsich@Winnies-MacBook-Pro.local>
2023-06-07 12:14:07 -0700

    HP Bar testing, manual

---
+++ Untitled Forest Game/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthBar : MonoBehaviour
+{
+    public TextMeshProUGUI healthText;
+    public Image healthBar;
+    float health = 100.0f;
+    float maxHealth = 100.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        healthText.text = "Health: " + health + "%";
+        HealthBarFiller();
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            Damage(20.0f);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Heal(10.0f);
+        }
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    void HealthBarFiller()
+    {
+        healthBar.fillAmount = health/maxHealth;
+    }
+
+    public void Damage(float damageAmount)
+    {
+        if (health > 0)
+        {
+            health -= damageAmount;
+        }
+    }
+
+    public void Heal(float healAmount)
+    {
+        if (health < maxHealth)
+        {
+            health += healAmount;
+        }
+    }
+}

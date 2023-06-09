ddcf184465af5e222dd323d9a8fd9835339c1a47
George Ortega <gjortega@ucdavis.edu>
2023-06-09 01:57:45 -0700

    Fixed character movement and added animation loop for moving

--- Untitled Forest Game/Assets/Scripts/CharacterMovement.cs
+++ Untitled Forest Game/Assets/Scripts/CharacterMovement.cs
@@ -7,11 +7,12 @@ namespace Embers
 	{
 		[SerializeField] private float gravityStrength;
 		[SerializeField] private float jumpStrength;
-		public bool useCharacterForward = false;
-		public bool lockToCameraForward = false;
-		public float turnSpeed = 10f;
-		public KeyCode sprintJoystick = KeyCode.JoystickButton2;
-		public KeyCode sprintKeyboard = KeyCode.Space;
+		[SerializeField] private float moveSpeed = 2f;
+		[SerializeField] private float turnSpeed = 10f;
+		[SerializeField] private KeyCode sprintJoystick = KeyCode.JoystickButton2;
+		[SerializeField] private KeyCode sprintKeyboard = KeyCode.LeftShift;
+		[SerializeField] private KeyCode jumpKeyboard = KeyCode.Space;
+
 
 		private float turnSpeedMultiplier;
 		private float speed = 0f;
@@ -36,51 +37,37 @@ namespace Embers
 
 		void Update()
 		{
-			// Vertical movement
-			Ray groundCheckRay = new Ray(transform.position, Vector3.down);
-
-			// Check if the player is on the ground
-			if (Physics.Raycast(groundCheckRay, 0.1f))
-			{
-				if (Input.GetKeyDown(KeyCode.Space))
-				{
-					rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
-				}
-			}
-		}
-
-		// Update is called once per frame
-		void FixedUpdate()
-		{
-#if ENABLE_LEGACY_INPUT_MANAGER
 			input.x = Input.GetAxis("Horizontal");
 			input.y = Input.GetAxis("Vertical");
 
 			// set speed to both vertical and horizontal inputs
-			if (useCharacterForward)
-				speed = Mathf.Abs(input.x) + input.y;
-			else
-				speed = Mathf.Abs(input.x) + Mathf.Abs(input.y);
+			speed = Mathf.Abs(input.x) + Mathf.Abs(input.y);
 
 			speed = Mathf.Clamp(speed, 0f, 1f);
 			speed = Mathf.SmoothDamp(anim.GetFloat("Speed"), speed, ref velocity, 0.1f);
 			anim.SetFloat("Speed", speed);
 
-			// Apply gravity to player
-			rb.AddForce(Physics.gravity * (gravityStrength - 1) * rb.mass);
-
-
-
-			if (input.y < 0f && useCharacterForward)
-				direction = input.y;
-			else
-				direction = 0f;
-
-			anim.SetFloat("Direction", direction);
-
-			// set sprinting
-			isSprinting = ((Input.GetKey(sprintJoystick) || Input.GetKey(sprintKeyboard)) && input != Vector2.zero && direction >= 0f);
+			isSprinting = Input.GetKey(sprintKeyboard) || Input.GetKey(sprintJoystick) && speed > 0f;
 			anim.SetBool("isSprinting", isSprinting);
+
+			speed = isSprinting ? 2 * speed : speed;
+
+
+			transform.Translate(targetDirection * speed * moveSpeed * Time.deltaTime, Space.World);
+			// Vertical movement
+			Ray groundCheckRay = new Ray(transform.position, Vector3.down);
+
+			// Check if the player is on the ground
+			if (Physics.Raycast(groundCheckRay, 0.5f))
+			{
+				if (Input.GetKeyDown(jumpKeyboard))
+				{
+					rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+				}
+			}
+
+			// Apply gravity to player
+			rb.AddForce(Physics.gravity * (gravityStrength - 1) * rb.mass * Time.deltaTime);
 
 			// Update target direction relative to the camera view (or not if the Keep Direction option is checked)
 			UpdateTargetDirection();
@@ -96,35 +83,19 @@ namespace Embers
 
 				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(euler), turnSpeed * turnSpeedMultiplier * Time.deltaTime);
 			}
-#else
-        InputSystemHelper.EnableBackendsWarningMessage();
-#endif
 		}
 
 		public virtual void UpdateTargetDirection()
 		{
-			if (!useCharacterForward)
-			{
-				turnSpeedMultiplier = 1f;
-				var forward = mainCamera.transform.TransformDirection(Vector3.forward);
-				forward.y = 0;
+			turnSpeedMultiplier = 1f;
+			var forward = mainCamera.transform.TransformDirection(Vector3.forward);
+			forward.y = 0;
 
-				//get the right-facing direction of the referenceTransform
-				var right = mainCamera.transform.TransformDirection(Vector3.right);
+			//get the right-facing direction of the referenceTransform
+			var right = mainCamera.transform.TransformDirection(Vector3.right);
 
-				// determine the direction the player will face based on input and the referenceTransform's right and forward directions
-				targetDirection = input.x * right + input.y * forward;
-			}
-			else
-			{
-				turnSpeedMultiplier = 0.2f;
-				var forward = transform.TransformDirection(Vector3.forward);
-				forward.y = 0;
-
-				//get the right-facing direction of the referenceTransform
-				var right = transform.TransformDirection(Vector3.right);
-				targetDirection = input.x * right + Mathf.Abs(input.y) * forward;
-			}
+			// determine the direction the player will face based on input and the referenceTransform's right and forward directions
+			targetDirection = input.x * right + input.y * forward;
 		}
 	}
 }

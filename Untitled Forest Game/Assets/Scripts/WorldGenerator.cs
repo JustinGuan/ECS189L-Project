f1ac606e0201b217c421a83ef19bda38606882fb
CennyBo <116618080+CennyBo@users.noreply.github.com>
2023-06-12 13:44:09 -0700

    Added multiple object arrays

--- Untitled Forest Game/Assets/Scripts/WorldGenerator.cs
+++ Untitled Forest Game/Assets/Scripts/WorldGenerator.cs
@@ -2,22 +2,35 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    [SerializeField] int worldSize = 100;
+    public int worldSize = 100;
     public float scale = 0.1f;
     public float heightMultiplier = 10f;
     public int octaves = 4;
     public float persistence = 0.5f;
     public float lacunarity = 2f;
 
-    public GameObject[] objectPrefabs;
-    public int maxObjects = 1000;
+    
+    public GameObject[] treePrefabs;
+    public GameObject[] rockPrefabs;
+    public GameObject[] mushroomPrefabs;
+    public GameObject[] grassPrefab;
+    public GameObject flamePrefab;
+    public int maxTrees = 100;
+    public int maxRocks = 100;
+    public int maxMushrooms = 100;
+    public int maxGrass = 100;
     private int mapEdge;
 
     private void Start()
     {
-        GenerateTerrain();
-        GenerateObjects();
         mapEdge = (int)(worldSize / 2f);
+        GenerateTerrain();
+
+        GenerateObjects(treePrefabs, maxTrees);
+        GenerateObjects(rockPrefabs, maxRocks);
+        GenerateObjects(mushroomPrefabs, maxMushrooms);
+        GenerateObjects(grassPrefab, maxGrass);
+        GenerateFlame();
     }
 
     private void GenerateTerrain()
@@ -36,11 +49,11 @@ public class WorldGenerator : MonoBehaviour
             for (int z = -mapEdge; z < mapEdge; z++)
             {
                 float height = CalculateHeight(x, z);
-                heightmap[x, z] = height;
+                heightmap[x + mapEdge, z + mapEdge] = height; // Offset by mapEdge to make starting index 0 instead of -500
             }
         }
 
-        terrainData.SetHeights(-mapEdge, -mapEdge, heightmap);
+        terrainData.SetHeights(0, 0, heightmap);
     }
 
     private float CalculateHeight(int x, int z)
@@ -64,16 +77,18 @@ public class WorldGenerator : MonoBehaviour
         return height * heightMultiplier;
     }
 
-    private void GenerateObjects()
+    // Can avoid overlaps if you keep an array of all the positions.
+    // If the random position chosen is to close to one that has already been used, choose a new one.
+    private void GenerateObjects(GameObject[] objectArray, int maxObjects)
     {
         for (int i = 0; i < maxObjects; i++)
         {
             Vector3 randomPosition = GetRandomPosition();
-            int randomIndex = Random.Range(0, objectPrefabs.Length);
-            GameObject prefab = objectPrefabs[randomIndex];
+            int randomIndex = Random.Range(0, objectArray.Length);
+            GameObject prefab = objectArray[randomIndex];
 
             Instantiate(prefab, randomPosition, Quaternion.identity);
-        }
+        }        
     }
 
     private Vector3 GetRandomPosition()
@@ -83,5 +98,13 @@ public class WorldGenerator : MonoBehaviour
         float y = Terrain.activeTerrain.SampleHeight(new Vector3(x, 0f, z));
 
         return new Vector3(x, y, z);
+    }
+
+    private void GenerateFlame()
+    {
+        float flameHeight = Terrain.activeTerrain.SampleHeight(new Vector3(0f, 0f, 0f));
+        Debug.Log(flameHeight);
+        Vector3 flamePosition = new Vector3(0f, flameHeight, 0f);
+        Instantiate(flamePrefab, flamePosition, Quaternion.identity);
     }
 }

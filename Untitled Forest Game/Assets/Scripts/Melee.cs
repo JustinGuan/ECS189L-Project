c75f76261f498bb72c49daaf5a73de4c9a1f0275
JustinGuan <102770588+JustinGuan@users.noreply.github.com>
2023-06-12 00:22:09 -0700

    Made the attack animation

--- Untitled Forest Game/Assets/Scripts/Melee.cs
+++ Untitled Forest Game/Assets/Scripts/Melee.cs
@@ -1,31 +1,34 @@
 using Embers;
 using UnityEngine;
 
-namespace embers
+namespace Embers
 {
     public class MeleeAttack : MonoBehaviour
     {
         public Animator animator;
+        [SerializeField] private KeyCode meleeKeyboard = KeyCode.Mouse0;
 
         public Transform attackPoint;
         public LayerMask enemyLayers;
+        private bool isAttack = false;
 
         public float attackRange = 0.5f;
         public int attackDamage = 40;
+
         public void Attack()
         {
-            //Attack animation
-            animator.SetTrigger("Attack");
+            // Attack animation
+            animator.Play("Attack");
 
-            //Detect enemies in attack range
+            // Detect enemies in attack range
             Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
 
-            //Damage
+            // Damage enemies
             foreach (Collider enemy in hitEnemies)
             {
-                // enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+                // Perform damage on the enemy
+                /*enemy.GetComponent<Enemy>().TakeDamage(attackDamage);*/
             }
-
         }
 
         private void OnDrawGizmosSelected()
@@ -33,16 +36,23 @@ namespace embers
             if (attackPoint == null)
                 return;
 
+            // Draw a wire sphere to visualize the attack range
             Gizmos.DrawWireSphere(attackPoint.position, attackRange);
         }
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetKeyDown(meleeKeyboard))
             {
+                //isAttack = true;
                 Attack();
+            }
+
+            else
+            {
+                //isAttack = false;
+                //animator.SetBool("isAttack", isAttack);
             }
         }
     }
-
 }

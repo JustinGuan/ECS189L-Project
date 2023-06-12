f76e732254e5e1c6462972a189b5015de79bba81
JustinGuan <102770588+JustinGuan@users.noreply.github.com>
2023-06-11 19:24:15 -0700

    Removed and fixed some assets

--- Untitled Forest Game/Assets/Scripts/ThirdPersonShooterController.cs
+++ Untitled Forest Game/Assets/Scripts/ThirdPersonShooterController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using StarterAssets;
 using UnityEngine.InputSystem;
 
 public class ThirdPersonShooterController : MonoBehaviour
